370d50aa484cf04512bc4a7404be956feb7812ff
Dmitriy Begishev <mintdragon@yandex.ru>
2022-04-25 21:52:02 +0400

    Product list is displayed on the page
    Problem: Product list is displayed on the page all products is displayed on the first opened list, when we clicked on the subcategories

--- static/scripts/product_list.js
+++ static/scripts/product_list.js
@@ -11,7 +11,28 @@ async function send_url(url, store_id, retailer) {
     .then(response => response.json()    
     .then(products => {
         console.log(products);  
+        rendering_markets_list(products);
 
     })    
     );
+}
+
+function rendering_markets_list(products) {
+    // функция создает ненумерованный список с продуктами
+    let div  = document.getElementById('products');
+    let ul = document.createElement('ul');
+    div.appendChild(ul);
+
+    for (let i = 0, ln = products.length; i < ln; i++) {
+        let li = document.createElement('li');
+        img_src = products[i].image_urls[0];
+        product_name = products[i].name;
+        price = products[i].price;
+        //market_src = markets[i].store_id;
+        //li.innerHTML = "<a href=\"/" + markets[i].retailer + "/sid=" + market_src + "\"><img src=\"" + img_src + "\" width=\"300px\" height=\"100px\" alt=\"market_logo\"></a>";
+        //li.innerHTML += '<div class="market">'+markets[i].name+'</div>';
+        li.innerHTML = product_name, price;
+        li.setAttribute('id', products[i].legacy_product_id);
+        ul.appendChild(li);
+    }
 }
