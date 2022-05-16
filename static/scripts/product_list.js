ec2d1e8a248e2e1e753efaaf7e90a1e6c53e8dcb
Dmitriy Begishev <mintdragon@yandex.ru>
2022-05-16 22:07:35 +0400

    got rid of .then

--- static/scripts/product_list.js
+++ static/scripts/product_list.js
@@ -1,22 +1,22 @@
+let products = [];
+let cart_list = [];
+
 async function send_url(url, store_id, category_id) {
     // функция отправляет на сервер store_id и canonical_url подкатегории с продуктами
     data = [url, store_id];
-    let response = await fetch('http://127.0.0.1:8080/send_product_list', {
-        method: 'POST', 
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(data)
-    })    
-    .then(response => response.json()    
-    .then(products => {
-        //console.log(products.find(item => item.price == 92.99));  
-        rendering_markets_cards(products, category_id);
-
-    })     
-    );
+    const response = await fetch('http://127.0.0.1:8080/send_product_list', {
+      method: 'POST', 
+      headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(data)
+  });
+    products = await response.json();  
+    rendering_markets_cards(products, category_id);
 }
-console.log(products);
+
+
+//console.log(products);
 function rendering_markets_cards(products, category_id) {
     // функция создает картточки с продуктами
     let div  = document.getElementById('products-' + category_id);
@@ -34,7 +34,7 @@ function rendering_markets_cards(products, category_id) {
                 data-id="${product.id}" 
                 data-name="${product.name}" 
                 data-price="${product.price}" 
-                onclick='add_to_cart(this.dataset.id, this.dataset.name, 
+                onclick='add_to_cart_list(this.dataset.id, this.dataset.name, 
                   this.dataset.price)'>
                 Добавить в корзину
               </button>
