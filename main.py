c614abdf4afad4b8427e2f8dd8bf6e516ca5d649
Dmitriy Begishev <mintdragon@yandex.ru>
2022-05-21 22:13:27 +0400

    Added checking the availability of products from the list in the other stores. Added discription to some funtions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 from urllib import response
+from operator import itemgetter
 
 from fastapi import FastAPI, Request, Response
 from fastapi.templating import Jinja2Templates
@@ -55,6 +56,8 @@ list_markets = []
 
 @app.get("/")
 async def get(request: Request):
+    ''' Главная страница
+    '''
     return templates.TemplateResponse(
         "index.html",
         {"request": request, 
@@ -63,8 +66,14 @@ async def get(request: Request):
 
 @app.post('/')
 async def add_address(request: Request):
+    ''' Функция запрашивает доступные магазины с доставкой по
+        указанному адресу
+    '''
+    
     coords = await request.json()
     markets = sb_p.get_markets(lat = coords[0], lon = coords[1])
+    global list_markets  # очищаем список перед каждым вызовом
+    list_markets = []
     for market in markets:
         raw_market = {
             'id' : market['id'],
@@ -109,10 +118,36 @@ async def compare_order(order, request: Request):
     '''Функция сравнивает заказ пользователя в разных магазинах
     '''
     order_list = order.split('&')
-    print(list_markets)
+    # добавляем в markets_true_set магазины в которых весь набор 
+    # продуктов имеется в наличии
+    markets_true_set = [] 
+    for market in list_markets: # проверяем все магазины
+        confirmed_prod = [market] # сразу добавляем инфу о магазине
+        total_price = 0 
+        for product in order_list: # проверяем наличие всех продуктов
+            # добавляем в confirmed_prod продукты, которые есть в 
+            # магазине. Потом если количество в order_list и 
+            # confirmed_prod совпадут, то добавляем магазин 
+            # в markets_true_set
+            try:
+                info =  p_other_stores.get(market.store_id, product)
+                if info['product']['offer']['price']:
+                    confirmed_prod.append(info)
+                    total_price += float(info['product']['offer']['price'])
+
+            except:
+                break
+        confirmed_prod.append(total_price)
+        if len(order_list) + 2 == len(confirmed_prod): # на 2 больше т.к.
+            # дополнительно идет информация о магазине и общая цена
+            markets_true_set.append(confirmed_prod)
+    # сортируем список по возрастанию общей цены
+    markets_true_set = sorted(markets_true_set, key=itemgetter(-1))
+
+        
     return templates.TemplateResponse(
                 "static/urls/compare.html",
                 {"request": request,
-                'order_list': order_list
+                "markets_true_set": markets_true_set,
                 } 
     )
