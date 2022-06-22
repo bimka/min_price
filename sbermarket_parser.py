880ac3e7467a1e00c79c1bab16fcbebc74ebfd26
Dmitriy Begishev <mintdragon@yandex.ru>
2022-06-22 21:53:54 +0400

    Updated module formation

--- sbermarket_parser.py
+++ sbermarket_parser.py
@@ -4,26 +4,26 @@ def get_markets(lat, lon):
     ''' Функция отправляет на сервер Сбермаркета координаты доставки,
         возвращается список магазинов, совершающих доставку на данный адрес
     '''
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0', 
+        'Accept': 'application/json, text/plain, */*', 
+        'Accept-Encoding': 'gzip, deflate, br', 
+        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3', 
+        'api-version': '3.0',
+        'client-token': '7ba97b6f4049436dab90c789f946ee2f', 
+        'Connection': 'keep-alive', 
+        'DNT': '1', 
+        'Host': 'sbermarket.ru',
+        'is-storefront-ssr': 'false', 
+        'Referer': 'https://sbermarket.ru/',
+        'Sec-Fetch-Dest': 'empty', 
+        'Sec-Fetch-Mode': 'cors', 
+        'Sec-Fetch-Site': 'same-origin',
+        'TE': 'trailers',             
+    }
     r = requests.get(
         f'https://sbermarket.ru/api/stores?lat={ lat }&lon={ lon }&include=closest_shipping_options,labels,retailer&shipping_method=delivery', 
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0', 
-            'Accept': 'application/json, text/plain, */*', 
-            'Accept-Encoding': 'gzip, deflate, br', 
-            'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3', 
-            'api-version': '3.0',
-            'client-token': '7ba97b6f4049436dab90c789f946ee2f', 
-            'Connection': 'keep-alive', 
-            'DNT': '1', 
-            'Host': 'sbermarket.ru',
-            'is-storefront-ssr': 'false', 
-            'Referer': 'https://sbermarket.ru/',
-            'Sec-Fetch-Dest': 'empty', 
-            'Sec-Fetch-Mode': 'cors', 
-            'Sec-Fetch-Site': 'same-origin',
-            'TE': 'trailers', 
-            
-        }
+        headers = headers,
         )
     return r.json()
 
