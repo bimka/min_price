c80e06ab0b40cfe9aa8ff01a47477e76a5cf3ae9
Dmitriy Begishev <mintdragon@yandex.ru>
2022-06-23 22:18:00 +0400

    Changed count of arguments get_markets funtion and added session connection
    Coordinates moved to the one COORDS argument and session's information to the CONNECTION argument. Added extra fields to the session header.

--- sbermarket_parser.py
+++ sbermarket_parser.py
@@ -1,31 +1,26 @@
 import requests
 
-def get_markets(lat, lon):
+def get_markets(CONNECTION, COORDS):
     ''' Функция отправляет на сервер Сбермаркета координаты доставки,
         возвращается список магазинов, совершающих доставку на данный адрес
     '''
-    headers={
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0', 
-        'Accept': 'application/json, text/plain, */*', 
-        'Accept-Encoding': 'gzip, deflate, br', 
-        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3', 
+    lat = COORDS[0]
+    lon = COORDS[1]
+
+    additional_header_fields={
         'api-version': '3.0',
         'client-token': '7ba97b6f4049436dab90c789f946ee2f', 
-        'Connection': 'keep-alive', 
-        'DNT': '1', 
         'Host': 'sbermarket.ru',
         'is-storefront-ssr': 'false', 
-        'Referer': 'https://sbermarket.ru/',
-        'Sec-Fetch-Dest': 'empty', 
-        'Sec-Fetch-Mode': 'cors', 
-        'Sec-Fetch-Site': 'same-origin',
-        'TE': 'trailers',             
     }
-    r = requests.get(
+    CONNECTION.headers.update(additional_header_fields)
+
+    response = requests.get(
         f'https://sbermarket.ru/api/stores?lat={ lat }&lon={ lon }&include=closest_shipping_options,labels,retailer&shipping_method=delivery', 
-        headers = headers,
+        headers = CONNECTION.headers,
         )
-    return r.json()
+
+    return response.json()
 
 if __name__ == "__main__":
     lat = 55.75503467371233
