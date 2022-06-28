628185898801093bd037c0b490b9b2d4e2201160
Dmitriy Begishev <mintdragon@yandex.ru>
2022-06-28 22:01:21 +0400

    Header from response changed to the session update
    Session's arguments don't save in the session

--- session.py
+++ session.py
@@ -1,5 +1,5 @@
 import requests
-import fake_useragent
+from fake_useragent import UserAgent
 
 def establish_connection():
     ''' Функция отправляет на сервер Сбермаркета запрос для 
@@ -9,19 +9,21 @@ def establish_connection():
 
     session = requests.Session()
 
-    user = fake_useragent.UserAgent().random
+    user_agent = UserAgent()
     
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-Language': 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
         'Connection': 'keep-alive',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent': user,
+        #'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0', 
+        'User-Agent': user_agent.random,
     }
     
+    session.headers.update(headers)
+
     responce = session.get(        
         url,
-        headers = headers,
         )
         
     add_csrf_token_to_cookies(session)
@@ -58,5 +60,5 @@ def add_csrf_token_to_cookies(session):
 
 if __name__ == "__main__":
     session = establish_connection()
-    print(session)
+    print(session.headers)
     
