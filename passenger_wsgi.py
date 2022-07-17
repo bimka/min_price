e63df2b7827a4005fd70879c7bd2f5c0a93f2828
Dmitriy Begishev <mintdragon@yandex.ru>
2022-07-17 21:43:02 +0400

    Change psth.insert

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,6 @@
-from main import app as application
+from main import app 
+import os, sys
+
+sys.path.insert(0, '/var/www/u1727349/data/www/mindeliveryprice.ru')
+sys.path.insert(1, '/var/www/u1727349/data/min_delivery_price_env/lib/python3.10/site-packages')
+application = app
